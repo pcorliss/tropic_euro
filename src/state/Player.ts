e4bd6591ea452cc1d1ce8c18c7fd044e197f1913
Philip Corliss <pcorliss@50projects.com>
2022-04-17 17:11:03 -0500

    Adding Player init and tests

--- src/state/Player.ts
+++ src/state/Player.ts
@@ -2,9 +2,15 @@ import { Board } from "./Board";
 
 export class Player {
     name: string;
-    victoryPoints: 0;
-    doubloons: 0;
-    goods: Record<string, number>;
+    victoryPoints = 0;
+    doubloons = 0;
+    goods: Record<"corn" | "indigo" | "sugar" | "tobacco" | "coffee", number> = {
+        corn: 0,
+        indigo: 0,
+        sugar: 0,
+        tobacco: 0,
+        coffee: 0,
+    };
     board: Board;
 
     constructor(name: string) {
--- test/state/Player.test.ts
+++ test/state/Player.test.ts
@@ -11,5 +11,21 @@ describe("Player", () => {
         it("inits a board", () => {
             expect(p.board.sanJuanColonists).toBe(0);
         });
+
+        it("inits a vps", () => {
+            expect(p.victoryPoints).toBe(0);
+        });
+
+        it("inits a doubloons", () => {
+            expect(p.doubloons).toBe(0);
+        });
+
+        it("inits goods", () => {
+            expect(p.goods.corn).toBe(0);
+            expect(p.goods.indigo).toBe(0);
+            expect(p.goods.sugar).toBe(0);
+            expect(p.goods.tobacco).toBe(0);
+            expect(p.goods.coffee).toBe(0);
+        });
     });
 });
