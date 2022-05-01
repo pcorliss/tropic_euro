48e5b229f7784e50ed5d9c4dbda868e661086339
Philip Corliss <pcorliss@50projects.com>
2022-04-30 22:39:19 -0500

    Builder Role Added

--- src/roles/Builder.ts
+++ src/roles/Builder.ts
@@ -2,8 +2,70 @@ import { Role } from "../state/Role";
 import { Action } from "../state/Action";
 import { GameState } from "../state/GameState";
 import { Player } from "../state/Player";
+import { Building } from "../state/Building";
 export class Builder extends Role {
     name = "Builder";
     description = "";
     phase = "building";
+
+    buildingCost(building: Building, player: Player, gs: GameState): number {
+        const quarryCount = player.board.plantations
+            .filter((p) =>
+                p.staffed &&
+                p.type == "quarry"
+            )
+            .length;
+        const maxQuarryUsage = Math.min(quarryCount, building.points);
+        const bonus = gs.currentPlayer() == player ? 1 : 0;
+        return Math.max(0, building.cost - bonus - maxQuarryUsage);
+    }
+
+    // TODO memoize the lookups by moving this to the player object
+    canBuy(building: Building, player: Player, gs: GameState): boolean {
+        const availableSpace = player.board.buildings.reduce((sum, b) => sum -= b.size, 12);
+        const cost = this.buildingCost(building, player, gs);
+        return player.doubloons >= cost && building.size <= availableSpace;
+    }
+
+    availableActions(gs?: GameState, player?: Player): Action[] {
+        const actions: Action[] = [];
+
+        if (gs.currentTurnPlayer() != player) {
+            return actions;
+        }
+
+        const seenBuildings = new Set<string>(player.board.buildings.map((b) => b.name));
+        gs.buildings.forEach((b) => {
+            if (!seenBuildings.has(b.name) && this.canBuy(b, player, gs)) {
+                actions.push(
+                    new Action(
+                        `buy${b.name.replace(/\s+/g, "")}`,
+                        (gs: GameState, player: Player): void => {
+                            gs.advancePlayer();
+                            // Relies on poor equality checking in JS
+                            gs.buildings = gs.buildings.filter((gsB) => b != gsB);
+                            player.board.buildings.push(b);
+                            player.doubloons -= this.buildingCost(b, player, gs);
+                            return;
+                        },
+                    )
+                );
+            }
+            seenBuildings.add(b.name);
+        });
+
+        if (actions.length > 0) {
+            actions.push(
+                new Action(
+                    "skip",
+                    (gs: GameState, player: Player): void => {
+                        gs.advancePlayer();
+                        return;
+                    },
+                )
+            );
+        }
+
+        return actions;
+    }
 }
---
+++ test/roles/Builder.test.ts
@@ -0,0 +1,184 @@
+import { GameState } from "../../src/state/GameState";
+import { Builder } from "../../src/roles/Builder";
+import { Role } from "../../src/state/Role";
+import { Player} from "../../src/state/Player";
+import { Plantation } from "../../src/state/Plantation";
+import { ConstructionHut } from "../../src/buildings/ConstructionHut";
+import { SmallIndigoPlant } from "../../src/buildings/SmallIndigoPlant";
+import { SmallMarket } from "../../src/buildings/SmallMarket";
+import { Fortress } from "../../src/buildings/Fortress";
+import { Wharf } from "../../src/buildings/Wharf";
+
+describe("Builder", () => {
+    let gs: GameState = null;
+    let role: Role = null;
+    let player: Player = null;
+
+    beforeEach(() => {
+        gs = new GameState(["Alice", "Bob", "Carol"]);
+        role = gs.availableRoles.find((r) => r instanceof Builder);
+        gs.currentRole = role;
+        player = gs.players[0];
+    });
+
+    describe("availableActions", () => {
+        it("returns nothing if the player is not the current turn player", () => {
+            const actions = role.availableActions(gs, gs.players[1]);
+            expect(actions).toHaveLength(0);
+        });
+
+        it("returns nothing if the player can't afford them", () => {
+            gs.currentTurnPlayerIdx = 1;
+            gs.players[1].doubloons = 0;
+            const actions = role.availableActions(gs, gs.players[1]);
+            expect(actions).toHaveLength(0);
+        });
+
+        it("returns buildings the player can afford", () => {
+            player.doubloons = 1;
+            const actions = role.availableActions(gs, player);
+            const actionKeys = actions.map((a) => a.key);
+            expect(actionKeys).toContain("buySmallMarket");
+            expect(actionKeys).toContain("buySmallIndigoPlant");
+        });
+
+        it("returns buildings the player can only afford via quarries", () => {
+            gs.currentTurnPlayerIdx = 1;
+            player = gs.players[1];
+            player.doubloons = 0;
+            player.board.plantations.push(new Plantation("quarry"));
+            player.board.plantations[1].staffed = true;
+            const actions = role.availableActions(gs, player);
+            const actionKeys = actions.map((a) => a.key);
+            expect(actionKeys).toContain("buySmallMarket");
+            expect(actionKeys).toContain("buySmallIndigoPlant");
+        });
+
+        it("ignores unstaffed quarries", () => {
+            gs.currentTurnPlayerIdx = 1;
+            player = gs.players[1];
+            player.doubloons = 0;
+            player.board.plantations.push(new Plantation("quarry"));
+            player.board.plantations[1].staffed = false;
+            const actions = role.availableActions(gs, player);
+            const actionKeys = actions.map((a) => a.key);
+            expect(actionKeys).not.toContain("buySmallMarket");
+            expect(actionKeys).not.toContain("buySmallIndigoPlant");
+        });
+
+        it("respects the max quarry discount", () => {
+            gs.currentTurnPlayerIdx = 1;
+            player = gs.players[1];
+            player.doubloons = 0;
+            player.board.plantations.push(new Plantation("quarry"));
+            player.board.plantations.push(new Plantation("quarry"));
+            player.board.plantations[1].staffed = true;
+            player.board.plantations[2].staffed = true;
+            const actions = role.availableActions(gs, player);
+            const actionKeys = actions.map((a) => a.key);
+            expect(actionKeys).not.toContain("buyConstructionHut");
+        });
+
+        it("doesn't return buildings the player already has", () => {
+            player.doubloons = 10;
+            player.board.buildings.push(new ConstructionHut);
+            const actions = role.availableActions(gs, player);
+            const actionKeys = actions.map((a) => a.key);
+            expect(actionKeys).not.toContain("buyConstructionHut");
+        });
+
+        it("doesn't return buildings that are sold out", () => {
+            player.doubloons = 10;
+            gs.buildings = gs.buildings.filter((b) => b.name != "Construction Hut");
+            const actions = role.availableActions(gs, player);
+            const actionKeys = actions.map((a) => a.key);
+            expect(actionKeys).not.toContain("buyConstructionHut");
+        });
+
+        it("only returns buildings if there's enough space", () => {
+            player.doubloons = 10;
+            // 12 spots on a board
+            for(let i=0; i < 11; i++) {
+                player.board.buildings.push(new SmallIndigoPlant);
+            }
+            const actions = role.availableActions(gs, player);
+            const actionKeys = actions.map((a) => a.key);
+            expect(actionKeys).toContain("buySmallMarket");
+            expect(actionKeys).not.toContain("buyFortress");
+        });
+
+        it("respects the builder bonus", () => {
+            player.doubloons = 0;
+            const actions = role.availableActions(gs, player);
+            const actionKeys = actions.map((a) => a.key);
+            expect(actionKeys).toContain("buySmallMarket");
+            expect(actionKeys).toContain("buySmallIndigoPlant");
+        });
+
+        describe("returns an action that", () => {
+            it("removes the building from the building list", () => {
+                gs.buildings = [new SmallMarket, new SmallMarket];
+                const actions = role.availableActions(gs, player);
+                const a = actions.find((a) => a.key == "buySmallMarket");
+                a.apply(gs, player);
+                expect(gs.buildings).toHaveLength(1); 
+             });
+
+            it("adds the building to the player's board", () => {
+                gs.buildings = [new SmallMarket];
+                const actions = role.availableActions(gs, player);
+                const a = actions.find((a) => a.key == "buySmallMarket");
+                a.apply(gs, player);
+                expect(player.board.buildings).toHaveLength(1); 
+            });
+
+            it("deducts money from the player", () => {
+                player.doubloons = 9;
+                gs.buildings = [new Fortress];
+                const actions = role.availableActions(gs, player);
+                const a = actions.find((a) => a.key == "buyFortress");
+                a.apply(gs, player);
+                expect(player.doubloons).toBe(0);
+            });
+
+            it("gives a discount from quarries and respects max discount and building bonus", () => {
+                // cost of 9 - 1 (bonus) - 3 quarries == 5
+                player.doubloons = 5;
+                gs.buildings = [new Wharf];
+                for(let i=0; i<4; i++) {
+                    player.board.plantations.push(new Plantation("quarry"));
+                    player.board.plantations.at(-1).staffed = true;
+                }
+                const actions = role.availableActions(gs, player);
+                const a = actions.find((a) => a.key == "buyWharf");
+                a.apply(gs, player);
+                expect(player.doubloons).toBe(0);
+            });
+
+            it("advances current turn player", () => {
+                const actions = role.availableActions(gs, player);
+                const a = actions[0];
+                a.apply(gs, player);
+                expect(gs.currentTurnPlayerIdx).toBe(1);
+            });
+
+            it("allows the player to not take a building", () => {
+                const actions = role.availableActions(gs, player);
+                const a = actions.find((a) => a.key == "skip");
+                const expected = player.board.buildings.length;
+                a.apply(gs, player);
+                expect(player.board.buildings.length).toBe(expected);
+            });
+
+            it("ends the role if all players have gone", () => {
+                gs.currentTurnPlayerIdx = 2;
+                gs.currentPlayerIdx = 0;
+                const actions = role.availableActions(gs, gs.players[2]);
+                const a = actions[0];
+                a.apply(gs, gs.players[2]);
+                expect(gs.currentPlayerIdx).toBe(1);
+                expect(gs.currentRole).toBeNull();
+            });
+        });
+    });
+});
