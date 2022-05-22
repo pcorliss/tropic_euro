d78ed92b12e1c6b2a911c048418b0b7f3c660327
Philip Corliss <pcorliss@50projects.com>
2022-05-21 19:04:56 -0500

    Adding migrations

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,33 @@
 import DatabaseConstructor, {Database} from "better-sqlite3";
 
+export type Migration = {
+    priority: number
+    migration: string
+}
+
 export class Db {
     static conn: Database;
 
     static init(): void {
-        Db.conn = new DatabaseConstructor(":memory:", { verbose: console.log });
+        Db.conn ||= new DatabaseConstructor(":memory:", { verbose: console.log });
+    }
+
+    static migrate(migrations: Migration[]): void {
+        const migrationsCreate = "CREATE TABLE IF NOT EXISTS migrations (priority INTEGER)";
+        Db.conn.prepare(migrationsCreate).run();
+        const migrationExists = Db.conn.prepare("SELECT 1 FROM migrations WHERE priority = ?");
+        const insertMigration = Db.conn.prepare("INSERT INTO migrations VALUES (?)");
+        migrations
+            .filter((m) => {
+                // undefined vs "1" to filter out existing migrations
+                return !migrationExists.get(m.priority);
+            })
+            .sort((a, b) => a.priority - b.priority)
+            .forEach((m) => {
+                Db.conn.prepare(m.migration).run();
+                insertMigration.run(m.priority);
+            });
+        return;
     }
 }
 // const db = new Database(":memory:", { verbose: console.log });
--- test/db.test.ts
+++ test/db.test.ts
@@ -1,11 +1,54 @@
-import { Db } from "../src/Db";
+import { Db, Migration } from "../src/Db";
 
 describe("Db", () => {
+    afterEach(() => {
+        Db.conn.close();
+        Db.conn = undefined;
+    });
+
     describe("init", () => {
         it("instantiates a db construction", () => {
             expect(Db.conn).toBeUndefined();
             Db.init();
             expect(Db.conn).not.toBeNull();
         });
+
+        it("doesn't init a new db if one is already connected", () => {
+            Db.init();
+            const conn = Db.conn;
+            Db.init();
+            expect(conn).toBe(Db.conn);
+        });
     });
+
+    describe("migrate", () => {
+        it("runs migrations", () => {
+            Db.init();
+            const a = { priority: 0, migration: "CREATE TABLE a (info text);" };
+            const b = { priority: 1, migration: "CREATE TABLE b (info text);" };
+
+            Db.migrate([a, b]);
+            expect(Db.conn.prepare("SELECT COUNT(*) FROM a").pluck().get()).toBe(0);
+            expect(Db.conn.prepare("SELECT COUNT(*) FROM b").pluck().get()).toBe(0);
+        });
+
+        it("runs migrations in priority order", () => {
+            Db.init();
+            const a = { priority: 0, migration: "CREATE TABLE a (info text);" };
+            const b = { priority: 1, migration: "ALTER TABLE a RENAME info TO cake;" };
+            Db.migrate([b, a]);
+        });
+
+        it("skips migrations that have been run before", () => {
+            Db.init();
+            const a = { priority: 0, migration: "CREATE TABLE a (info text);" };
+            const b = { priority: 1, migration: "ALTER TABLE a RENAME info TO cake;" };
+            Db.migrate([a]);
+            Db.migrate([a,b]);
+        });
+    });
+
+    // describe("insert", () => {
+
+    // });
 });
