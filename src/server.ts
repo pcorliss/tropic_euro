44ebe243f31bfab65d399d2ab0cee193fcc488bb
Philip Corliss <pcorliss@50projects.com>
2022-06-25 21:41:05 -0500

    Adopt graphql scalars

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,7 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "graphql";
 import * as fs from "fs";
-import GraphQLJSON from "graphql-type-json";
-
+import { resolvers as scalarResolvers } from "graphql-scalars";
 
 import { GameState } from "./state/GameState";
 import { Db } from "../src/Db";
